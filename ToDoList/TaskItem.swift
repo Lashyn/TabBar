866d4cc35fabd9a439bf8947ce5ec7b5ec2d3501
Lashyn <admin@Lashyn-Galbiyatuly-2.local>
2024-03-18 09:57:13 +0500

    ToDoList

---
+++ ToDoList/TaskItem.swift
@@ -0,0 +1,15 @@
+//
+//  TaskItem.swift
+//  ToDoList
+//
+//  Created by Admin on 30.01.2024.
+//
+
+import Foundation
+
+struct TaskItem: Codable {
+    
+    var name = ""
+    var isComplete = false
+    
+}
